6d5d03e8a063af8d9240b381c40c8a2ddee60d4e
Funsho007 <yinkaoke007@gmail.com>
2024-07-16 19:32:52 -0500

    brand and customers

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -9,10 +9,14 @@ ActiveAdmin.register Customer do
   #
   # or
   #
+  #
+  #
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :primary_address, :primary_city, :primary_postal_code, :primary_province_id
+  #
   # permit_params do
   #   permitted = [:name, :email]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
