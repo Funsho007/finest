6794d9ee312d769f5abeeda969209685f62f9a48
Funsho007 <yinkaoke007@gmail.com>
2024-07-16 19:42:56 -0500

    province validation

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,6 @@
 class Province < ApplicationRecord
+  has_many :customers
+
+  validates :name, :tax_type, presence: true
+  validates :GST, :PST, :HST, numericality: { greater_than_or_equal_to: 0 }
 end
